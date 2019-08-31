6280f94ba68f5b2959149f1358dbace7dbaa5aeb
Salifu Mutaru <salifumutaru@gmail.com>
2019-08-31 14:37:13 +0000

    db envs

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,12 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.POSTGRESQLCONNSTR_DB,
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
     migrations: {
       directory: `${__dirname}/db/migrations`,
     },
